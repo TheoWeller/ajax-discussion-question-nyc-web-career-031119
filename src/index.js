beb8b9918ef7375731add5ceee322a90876a411e
Theo Weller <weller.theodor@gmail.com>
2019-04-25 14:17:11 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ const fullname = document.getElementById("fullname");
 console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
 
 document.addEventListener("DOMContentLoaded", () => {
+
+  function fetchData(){
+          var data = fetch('https://randomuser.me/api/')
+            .then( res => res.json() )
+            .then( json => console.log(json) )
+  }
+
+  fetchData()
+  
   console.log("CONTENT LOADED!");
 });
